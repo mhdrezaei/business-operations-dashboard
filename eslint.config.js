5a6a578dc16f31d46b9ac778a77531faca1abb16
Arman Nasiri <armannasiri98@gmail.com>
2025-11-05 15:50:05 +0330

    feat: add Persian language support and font assets

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,15 @@ import antfu from "@antfu/eslint-config";
 
 export default antfu({
 	react: true,
+	ignores: [
+		"**/*.css",
+		"**/fonts/**",
+		"**/*.woff",
+		"**/*.woff2",
+		"**/*.eot",
+		"**/*.ttf",
+		"**/*.zip",
+	],
 	rules: {
 		"style/quotes": ["error", "double"],
 		"style/semi": ["error", "always"],
