51324cf1ab3114dccf2950693931ecf25632d2eb
Raha ShahinParvaz <Rahashahinparvaz@gmail.comgit>
2026-06-18 19:16:37 +0330

    fix: ndp-431 Fix and resolve prevent line and pie charts from making API calls

--- src/pages/home/components/pie-chart.tsx
+++ src/pages/home/components/pie-chart.tsx
@@ -1,6 +1,6 @@
 import type { PieDataType } from "#src/api/home";
 import type { EChartsOption } from "echarts";
-import { fetchPie } from "#src/api/home";
+// import { fetchPie } from "#src/api/home";
 import { Card, Segmented } from "antd";
 import ReactECharts from "echarts-for-react";
 
@@ -9,18 +9,18 @@ import { useTranslation } from "react-i18next";
 
 export default function PieChart() {
 	const { t } = useTranslation();
-	const [data, setData] = useState<PieDataType[]>([]);
+	const [data] = useState<PieDataType[]>([]);
 	const [value, setValue] = useState<string | number>(
 		t("home.allChannels"),
 	);
 
-	const DATA_KEY = {
-		electronics: t("home.electronics"),
-		home_goods: t("home.homeGoods"),
-		apparel_accessories: t("home.apparelAccessories"),
-		food_beverages: t("home.foodBeverages"),
-		beauty_skincare: t("home.beautySkincare"),
-	};
+	// const DATA_KEY = {
+	// 	electronics: t("home.electronics"),
+	// 	home_goods: t("home.homeGoods"),
+	// 	apparel_accessories: t("home.apparelAccessories"),
+	// 	food_beverages: t("home.foodBeverages"),
+	// 	beauty_skincare: t("home.beautySkincare"),
+	// };
 
 	const option: EChartsOption = {
 		title: {
@@ -70,17 +70,17 @@ export default function PieChart() {
 
 	useEffect(() => {
 		if (value) {
-			fetchPie({ by: value }).then(({ result }) => {
-				setData(
-					result.map((item) => {
-						const code = item.code as keyof typeof DATA_KEY;
-						return {
-							...item,
-							name: DATA_KEY[code],
-						};
-					}),
-				);
-			});
+			// fetchPie({ by: value }).then(({ result }) => {
+			// 	setData(
+			// 		result.map((item) => {
+			// 			const code = item.code as keyof typeof DATA_KEY;
+			// 			return {
+			// 				...item,
+			// 				name: DATA_KEY[code],
+			// 			};
+			// 		}),
+			// 	);
+			// });
 		}
 	}, [value]);
 
