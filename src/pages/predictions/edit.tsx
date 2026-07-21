9aaf81b50d61ce471faeb0d7f77bf48aaa17f74a
Raha ShahinParvaz <Rahashahinparvaz@gmail.comgit>
2026-07-21 11:20:47 +0330

    fix: add prediction edit page

---
+++ src/pages/predictions/edit.tsx
@@ -0,0 +1,7 @@
+import PredictionListPage from "#src/features/prediction/list/ui/PredictionListPage";
+
+function EditPredictionPage() {
+	return <PredictionListPage />;
+}
+
+export default EditPredictionPage;
